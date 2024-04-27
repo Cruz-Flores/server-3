9a0b295894e941408460b45370f8ad54fac369c8
Yamile Velazquez <yamilevelazquezgarcia@gmail.com>
2024-04-27 12:46:48 -0600

    se obtine un dog

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const catsRouter = require('./controllers/cats');
 const middleware = require('./utils/middleware');
+const dogsRouter = require('./controllers/dogs');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/cats', catsRouter);
+app.use('/api/dogs', dogsRouter)
 
 app.use(middleware.unknownEndpoint);
 
--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,10 +1,18 @@
 const dogsRouter = require('express').Router();
 
 const dogs = [
-  { name: 'Rex', age: 4 },
-  { name: 'Spot', age: 3 },
+  {id :1, name: 'Rex', age: 4 },
+  {id:2, name: 'Spot', age: 3 },
 ];
 
 dogsRouter.get('/', (req, res) => {
   res.json(dogs);
 });
+
+dogsRouter.get('/:id', (req, res) =>{
+ const id = req.params.id;
+ const dog = dogs.find((dog) => dog.id === Number(id));
+ res.json(dog);
+})
+
+module.exports = dogsRouter;
