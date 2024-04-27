2740fb20ab6848ac7815efd2d4d661304b006fb4
Cruz Flores <cruz.tag@gmail.com>
2024-04-27 12:16:43 -0600

    feat: add GET endpoint for dogs in dogs.js

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,1 +1,10 @@
 const dogsRouter = require('express').Router();
+
+const dogs = [
+  { name: 'Rex', age: 4 },
+  { name: 'Spot', age: 3 },
+];
+
+dogsRouter.get('/', (req, res) => {
+  res.json(dogs);
+});
